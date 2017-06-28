5114c7277265f089057925eb2b42b3b4d5ab6a11
Ian Duke <ian.c.duke@gmail.com>
2017-06-28 02:39:07 +0000

    Completed exercise: Component Lifecycle Methods in action

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      currentUser: {name: "Bob"},  // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [
         {
           id: 1,
@@ -26,13 +26,21 @@ export default class App extends Component {
   // Called after component is rendered and attached to DOM, but not yet visible.
   componentDidMount() {
     console.log("componentDidMount invoked, commencing 3 second delay")
+    console.log("Simulating incoming message");
+  // Add a new message to the list of messages in the data store
+    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    const messages = this.state.messages.concat(newMessage);
+  // Update the state of the app component.}
+  // Calling setState will trigger a call to render() in App and all child components.
     setTimeout(() => {
-      this.setState( /* PLACE HOLDER */ )
+      this.setState( {messages: messages} )
     }, 3000);
   };
 
   render() {
     console.log("Rendering <App />");
+    /* Passing messages array from Lines 11-22 to MessageList component on Line 49
+       Passing currentUser from Line 10 to ChatBar component on Line 50 */
     return (
       <div>
         <nav className="navbar">
